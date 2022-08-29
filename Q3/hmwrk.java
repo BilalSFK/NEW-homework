d722603b871a5f8168ee3b0f87d1bf452585a49d
BİLAL <bllsfk.3984@gmail.com>
2022-08-29 15:04:16 +0300

    Bilal has committed

---
+++ Q3/hmwrk.java
@@ -0,0 +1,13 @@
+package Q3;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+    public class hmwrk {
+        public static void main(String[] args) {
+            String date= LocalDate.parse("2014-05-04").
+                    format(DateTimeFormatter.ISO_DATE_TIME);
+            System.out.println(date);
+        }
+    }
+
